6b1861a6a2b2460e6d7a7db2cec7541012fd7796
hungnm2904 <hungnm2904@gmail.com>
2017-12-23 14:53:00 +0700

    Set up dynamic port and script start for heroku deployment

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.set('view engine', 'hbs');
@@ -23,6 +24,6 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
